97da226df0d065cadfed3cf574adb66b8cb4a973
rafnuss <rafnuss@gmail.com>
2021-12-14 16:53:42 -0500

    Update demo and many modification to functions

--- R/geopressure.R
+++ R/geopressure.R
@@ -1,106 +1,283 @@
-#' Download
+#' Request and download mismatch maps of pressure
+#'
+#' This function return the mismatch map of atmospheric pressure measured by a
+#' geolocator (`PAM_data`). It performs the following actions: (1) Send a query
+#' to produce the Google Earth Engine (GEE) url of the code producing the maps
+#' for each stationary periods separately, (2) then read these map (geotiff) in
+#' a raster and (3) compute the likelihood map from the mismatch
+#'
+#' @param PAM_data PAM logger dataset list (see https://github.com/KiranLDA/PAMLr for details). This
+#' @param extent Geographical extend of the map to query as a list ordered by West,East,South,North  (e.g. c(-6,43,0,47))
+#' @param scale Number of pixel per latitude, longitude. 10 for a resoltion of 0.1° (~10) and 4 for a resolution of 0.25° (~30km). To avoid interpolating the ERA5 data, scale should be smaller than 10. Read more about scale on Google earth Engine documention.
+#' @param maxSample The computation of the mismatch is only performed on `maxSample` datapoints of pressure to reduce computational time. The samples are randomly (uniformly) selected on the timeserie.
+#' @param margin The margin is used in the threshold map to accept some measurement error. unit in meter. (1hPa~10m)
+#' @return List of raster map
+#' @export
+geopressure.map <-
+  function(pressure,
+           extent,
+           scale = 10,
+           maxSample = 250,
+           margin = 30) {
+    # Check input
+    testthat::expect_type(pressure, "list")
+    testthat::expect_true("date" %in% names(pressure))
+    testthat::expect_is(pressure$date, "POSIXt")
+    testthat::expect_true("obs" %in% names(pressure))
+    testthat::expect_is(pressure$obs, c("integer", 'numeric'))
+    testthat::expect_true("class" %in% names(pressure))
+    testthat::expect_is(pressure$class, "logical")
+    testthat::expect_true("staID" %in% names(pressure))
+    testthat::expect_length(pressure$obs, length(pressure$date))
+    testthat::expect_length(pressure$class, length(pressure$date))
+    testthat::expect_length(pressure$staID, length(pressure$date))
+    testthat::expect_is(extent, c("integer", 'numeric'))
+    testthat::expect_length(extent, 4)
+    testthat::expect_true(extent[1] >= -180 & extent[1] <= 180)
+    testthat::expect_true(extent[2] >= -180 & extent[2] <= 180)
+    testthat::expect_true(extent[3] >= -90 & extent[3] <= 90)
+    testthat::expect_true(extent[4] >= -90 & extent[4] <= 90)
+    testthat::expect_true(extent[1] < extent[2])
+    testthat::expect_true(extent[3] < extent[4])
+    testthat::expect_is(scale, c("integer", 'numeric'))
+    testthat::expect_gt(scale, 0)
+    testthat::expect_lte(scale, 10)
+    testthat::expect_is(maxSample, c("integer", 'numeric'))
+    testthat::expect_gt(maxSample, 0)
+    testthat::expect_is(margin, c("integer", 'numeric'))
+    testthat::expect_gte(margin, 0)
+
+    # convert from hPa to Pa
+    pres = pressure$obs * 100
+
+
+    # remove outliar as labeled in TRAINSET
+    pres[!is.na(pressure$class)] = NA
+
+    # remove flight period
+    pres[pressure$staID == 0] = NA
+
+    # smooth the data to 1hr
+    # need to be done
+
+    # downscale to 1hour
+    pres[format(pressure$date, "%M") != "00"] = NA
+
+    # remove stationary period with NA
+    pres[is.na(pressure$staID)] = NA
+
+    # Format query
+    bodyDF <- list(
+      time = jsonlite::toJSON(as.numeric(as.POSIXct(pressure$date[!is.na(pres)]))),
+      label = jsonlite::toJSON(pressure$staID[!is.na(pres)]),
+      pressure = jsonlite::toJSON(pres[!is.na(pres)]),
+      W = extent[1],
+      S = extent[3],
+      E = extent[2],
+      N = extent[4],
+      scale = scale,
+      maxSample = maxSample,
+      margin = margin
+    )
+
+    # Request URLS
+    message("Sending requests...")
+    res <-
+      httr::POST("http://glp.mgravey.com:24853/GeoPressure/v1/map/", body = bodyDF) #httr::verbose()
+
+    # check that the response is successful
+    if (!httr::content(res)$status == "success") {
+      message(httr::content(res))
+      stop('Error with request')
+    } else {
+      # Get URIS
+      # print(httr::content(res))
+      uris = unlist(httr::content(res)$data$urls)
+      labels = unlist(httr::content(res)$data$labels)
+      message(
+        'Request generated successfully for ',
+        length(labels),
+        ' stationary periods (',
+        sprintf('%d, ', sort(labels)),
+        ')'
+      )
+    }
+
+    # Perform the call in parallel
+    # GEE allows up to 12 requests at the same time, so we set the worker to 10
+    future::plan(future::multisession, workers = 10)
+    f = c()
+    message("Starting download:")
+    for (i_u in 1:length(uris)) {
+      f[[i_u]] <- future::future({
+        raster::brick(uris[i_u])
+      }, seed = TRUE)
+      progress(i_u, max = length(uris))
+    }
+
+    # Get the raster
+    rasterList = c()
+    message("Receiving download (geotiff):")
+    for (i_u in 1:length(uris)) {
+      rasterList[[i_u]] <- future::value(f[[i_u]])
+      progress(i_u, max = length(uris))
+
+      # convert MSE from Pa to hPa
+      values(rasterList[[i_u]][[1]]) <-
+        values(rasterList[[i_u]][[1]]) / 100 / 100
+
+      # Writing some metadata
+      metadata(rasterList[[i_u]]) <- list(
+        staID = labels[i_u],
+        nbSample = sum(bodyDF$label == labels[i_u]),
+        maxSample = maxSample,
+        extendSample = c(min(pressure$date[!is.na(pres) &
+                                             pressure$staID == labels[i_u]]), max(pressure$date[!is.na(pres) &
+                                                                                                  pressure$staID == labels[i_u]])),
+        margin = margin
+      )
+    }
+
+    # return
+    rasterList
+  }
+
+
+
+
+
+
+
+
+#' Convert MSE to probability
 #'
 #' This function
 #'
-#' @param PAM_data PAM logger dataset list
-#' @param file.location Path to the folder where the labeled files should be saved
-#' @param file.name Name for the file (default is PAM_data$id)
-#' @return PAM logger dataset list updated
+#' @param rasterList list of raster loaded from `geopressure.map()`
+#' @param s standardeviation of the pressure error
+#' @param thr threashold of the percentage of datapoint outside the elevation range to be considered not possible
+#' @return List of the probability raster map
 #' @export
-geopressure.request <- function(pressure, extent, scale=10, maxSample=250, margin=30){
+geopressure.Probmap <- function(rasterList, s = 1, thr = 0.9) {
+  rasterProbList = c()
+  for (i_s in 1:length(rasterList)) {
+    # get metadata
+    mt = metadata(rasterList[[i_s]])
 
-  # Check input
+    # compute Log-linear pooling weight
+    # pres_n = min(nbSample, mt$maxSample)
+    pres_n = as.numeric(difftime(mt$extendSample[2], mt$extendSample[1], units = "hours"))
+    w = log(pres_n) - 1
 
+    # get MSE layer
+    rasterProbList[[i_s]]  = rasterList[[i_s]][[1]]
+    # change 0 (water) in NA
+    rasterProbList[[i_s]][rasterProbList[[i_s]] == 0] = NA
+    # compute probability with equation
+    rasterProbList[[i_s]] <-  exp(-w * rasterProbList[[i_s]] / (s ^ 2))
+    # mask value of threashold
+    rasterProbList[[i_s]] <- rasterProbList[[i_s]] * (rasterList[[i_s]][[2]] > thr)
 
-  # convert from hPa to Ph
-  pres = pressure$obs * 100;
-
-  # remove outliar as labeled in TRAINSET
-  pres[!is.na(pressure$class)] = NA
-
-  # remove flight period
-  pres[pressure$staID==0] = NA
-
-  # smooth the data to 1hr
-
-  # downscale to 1hour
-  pres[format(pressure$date,"%M")!="00"] = NA
-
-  # Format query
-  bodyDF <- list(
-    time = jsonlite::toJSON(as.numeric(as.POSIXct(pressure$date[!is.na(pres)]))),
-    label = jsonlite::toJSON(pressure$staID[!is.na(pres)]),
-    pressure = jsonlite::toJSON(pres[!is.na(pres)]),
-    W=extent[1],
-    S=extent[3],
-    E=extent[2],
-    N=extent[4],
-    scale=scale,
-    maxSample=maxSample,
-    margin=margin
-  )
-
-  # Assert input
-
-
-  # Request URLS
-  print("Sending requests...",row.names = F)
-  res <- httr::POST("http://glp.mgravey.com/GeoPressure/v1/map", body = bodyDF) #httr::verbose()
-
-  # check that the response is successful
-  if (!httr::content(res)$status=="success"){
-    stop('Error with request')
-  } else {
-    # Get URIS
-    uris = unlist(httr::content(res)$data$urls)
-    print(paste0('Request generated successfully. ',length(uris),' URLs received.'),row.names = F)
+    # plot(rasterProbList[[i_s]])
+    metadata(rasterProbList[[i_s]]) <- metadata(rasterList[[i_s]])
   }
-
-  # Perform the call in parrallel
-  # GEE allows up to 12 requests at the same time, so we set the worker to 10
-  future::plan(future::multisession, workers = 10)
-  f = c()
-  print("Starting download:",row.names = F)
-  for (i_u in 1:length(uris)) {
-    f[[i_u]] <- future::future({raster::stack(uris[i_u])},seed=TRUE)
-    progress(i_u, max = length(uris))
-  }
-
-  # Get the raster
-  v=c()
-  print("Receiving download (geotiff):",row.names = F)
-  for (i_u in 1:length(uris)) {
-    v[[i_u]] <- future::value(f[[i_u]])
-    progress(i_u, max = length(uris))
-
-    # convert MSE from Pa to hPa
-    values(v[[i_u]][[1]]) <- values(v[[i_u]][[1]]) / 100 / 100
-
-    # compute number of datapoints
-    pres_n = sum(pressure$staID[!is.na(pres)]==i_u)
-
-
-    # Convert MSE in prob
-    w = log(pres_n)-1;
-
-    #
-    s = 1
-
-    #
-    tmp <- v[[i_u]][[1]]
-    values(tmp)[values(tmp)==0] <- NA
-    values(tmp) <-  exp(-w*values(tmp)/(s^2))
-    v[[i_u]] = addLayer(v[[i_u]], tmp)
-
-    # Writing metadata
-    metadata(v[[i_u]]) <- list(i_u,pres_n,w)
-  }
-
-  # return (such not intuitive to leave a variable out at the end to define what to return... )
-  v
+  rasterProbList
 }
 
 
+
+
+
+
+#' Request and download surface pressure timeseries at location
+#'
+#' This function return the surfrace atmospheric pressure timeseries from ERA5
+#' at a particualy location specify by lat and lon. I uses SRTM-30 to translate
+#' the pressure for the exact elevation of the ground level, accounting for
+#' both temporal varation of pressure and temperature.
+#'
+#' If you supply the pressure (and time) of the geolocator, it will additionally
+#' return the elevation of the geolocator above sea level considering that the bird was located at the location specify
+#'
+# The timeserie of the response will be on the same as time if supply, otherwise, it will return on a hourly basis between `startTime` and `endTime`.
+#'
+#' @param lon longitude to query (-180° to 180°).
+#' @param lat latitude to query (0° to 90°).
+#' @param pressure geolocator pressure structure (time and pressure) (optional)
+#' @param statTime if pressure not provided, then the starttime of the timeserie return is needed
+#' @param endTime same as starttime
+#' @return Timeserie of date, pressure and optionally altitude
+#' @export
+geopressure.timeseries <-
+  function(lon,
+           lat,
+           pressure = NULL,
+           endTime = NULL,
+           startTime = NULL) {
+    #Test
+    testthat::expect_is(lon, "numeric")
+    testthat::expect_is(lat, "numeric")
+    testthat::expect_true(lon >= -180 & lon <= 180)
+    testthat::expect_true(lat >= -90 & lat <= 90)
+    if (!is.null(pressure)) {
+      testthat::expect_type(pressure, "list")
+      testthat::expect_true("date" %in% names(pressure))
+      testthat::expect_is(pressure$date, "POSIXt")
+      testthat::expect_true("obs" %in% names(pressure))
+      testthat::expect_is(pressure$obs, c("numeric", 'integer'))
+      testthat::expect_length(pressure$obs, length(pressure$date))
+      endTime = NULL
+      startTime = NULL
+    } else {
+      testthat::expect_is(endTime, "POSIXt")
+      testthat::expect_is(startTime, "POSIXt")
+      restthat::expect_gt(endTime, startTime)
+    }
+
+    # Format query
+    bodyDF <- list(lon = lon,
+                   lat = lat)
+    if (!is.null(pressure)) {
+      bodyDF$time = jsonlite::toJSON(as.numeric(as.POSIXct(pressure$date)))
+      bodyDF$pressure = jsonlite::toJSON(pressure$obs * 100)
+    } else {
+      bodyDF$startTime = jsonlite::toJSON(as.numeric(as.POSIXct(startTime)))
+      bodyDF$endTime = jsonlite::toJSON(as.numeric(as.POSIXct(endTime)))
+    }
+
+    # Request URLS
+    message("Sending requests...")
+    res <-
+      httr::POST("http://glp.mgravey.com:24853/GeoPressure/v1/timeseries",
+                 body = bodyDF) #httr::verbose()
+
+    # check that the response is successful
+    if (!httr::content(res)$status == "success") {
+      print(httr::content(res))
+      stop('Error with request')
+    } else {
+      message('Request generated successfully.')
+    }
+    message('Downloading csv data.')
+    out = read.csv(httr::content(res)$data$url)
+
+    # convert Pa to hPa
+    out$pressure = out$pressure / 100
+
+    # convert time into date
+    out$time = as.POSIXct(out$time, origin = "1970-01-01")
+    names(out)[names(out) == 'time'] <- 'date'
+
+    # return
+    out
+  }
+
+
+
+
+
+
+# Progress bar function
 progress <- function (x, max = 100) {
   percent <- x / max * 100
   cat(sprintf('\r[%-50s] %d / %d',
--- R/trainset.R
+++ R/trainset.R
@@ -1,33 +1,42 @@
-#' Edit classification of activity and pressure with TRAINSET
+#' Edit classification of activity and pressure
 #'
-#' This function
+#' This function perform three steps: (1) write the csv file of the
+#' automatically labeled activity and pressure with `trainset.write`, (2) open
+#' trainset in your broweser (https://trainset.geocene.com/) so that you can
+#' edit the labels and (3) read the exported csv file from trainset with
+#' `trainset.read`.
 #'
-#' @param PAM_data PAM logger dataset list
+#' @param PAM_data PAM logger dataset list (see https://github.com/KiranLDA/PAMLr for details)
 #' @param file.location Path to the folder where the labeled files should be saved
-#' @param file.name Name for the file (default is PAM_data$id)
-#' @return PAM logger dataset list updated
+#' @param file.name Name for the file.
+#' @return PAM logger dataset list updated with the new label named `class` (`PAM_data$pressure$class` and `PAM_data$acceleration$class`)
 #' @export
 trainset.edit <- function(PAM_data,
                           file.location,
-                          file.name = paste0(PAM_data$id,'_act_pres')
-                          ){
+                          file.name = paste0(PAM_data$id, '_act_pres')) {
+
   # Create file from PAM_data if no labeled file for this bird exist
-  if (!file.exists(paste0(file.location,file.name,"-labeled.csv"))){
+  if (!file.exists(paste0(file.location, file.name, "-labeled.csv"))) {
     trainset.write(PAM_data,
-                  file.location,
-                  file.name)
+                   file.location,
+                   file.name)
 
     # Edit the file in browser
     browseURL("https://trainset.geocene.com/")
 
     # When labilization finished and new file saved, press enter
-    cond=T
-    while(cond){
-      readline(prompt="Press [enter] to continue")
-      if (!file.exists(paste0(file.location,file.name,"-labeled.csv"))){
-        cond=F
+    cond = T
+    while (cond) {
+      readline(prompt = "Press [enter] to continue")
+      if (!file.exists(paste0(file.location, file.name, "-labeled.csv"))) {
+        cond = F
       } else {
-        warning(paste0("No labelized file found. Make sure you exported the file from trainset as ",paste0(file.location,file.name,"-labeled.csv")))
+        warning(
+          paste0(
+            "No labelized file found. Make sure you exported the file from trainset as ",
+            paste0(file.location, file.name, "-labeled.csv")
+          )
+        )
       }
     }
   }
@@ -38,36 +47,107 @@ trainset.edit <- function(PAM_data,
                 file.name)
 }
 
+
+
+#' Write classification of activity and pressure
+#'
+#' This function writes the csv file of the automatically labeled activity and pressure which can be read with TRAINSET (https://trainset.geocene.com/).
+#'
+#' @param PAM_data PAM logger dataset list (see https://github.com/KiranLDA/PAMLr for details)
+#' @param file.location Path to the folder where the labeled files should be saved
+#' @param file.name Name for the file.
 #' @export
 trainset.write <- function(PAM_data,
                            file.location,
-                           file.name = paste0(PAM_data$id,'_act_pres')
-                               ){
-  write.csv(rbind(
-    data.frame(
-      series = "acceleration",
-      timestamp = strftime(PAM_data$acceleration$date , "%Y-%m-%dT%H:%M:%S%z"),
-      value = PAM_data$acceleration$act,
-      label = PAM_data$acceleration$class
+                           file.name = paste0(PAM_data$id, '_act_pres')) {
+
+  # Perform test
+  testthat::expect_type(PAM_data, "list")
+  testthat::expect_true("pressure" %in% names(PAM_data))
+  testthat::expect_type(PAM_data$pressure, "list")
+  testthat::expect_true("date" %in% names(PAM_data$pressure))
+  testthat::expect_true("obs" %in% names(PAM_data$pressure))
+  testthat::expect_true("acceleration" %in% names(PAM_data))
+  testthat::expect_type(PAM_data$acceleration, "list")
+  testthat::expect_true("date" %in% names(PAM_data$acceleration))
+  testthat::expect_true("act" %in% names(PAM_data$acceleration))
+  testthat::expect_true(dir.exists(file.location))
+  testthat::expect_type(file.location, "character")
+  testthat::expect_type(file.name, "character")
+  # create path if does not exit
+  if(!dir.exists(file.location)) {
+    dir.create(file.location)
+  }
+  testthat::expect_true(dir.exists(file.location))
+
+  # write a combined data.frame of pressure and acceleration in csv.
+  write.csv(
+    rbind(
+      data.frame(
+        series = "acceleration",
+        timestamp = strftime(PAM_data$acceleration$date , "%Y-%m-%dT%H:%M:%S%z"),
+        value = PAM_data$acceleration$act,
+        label = PAM_data$acceleration$class
+      ),
+      data.frame(
+        series = "pressure",
+        timestamp = strftime(PAM_data$pressure$date , "%Y-%m-%dT%H:%M:%S%z"),
+        value = PAM_data$pressure$obs,
+        label = ""
+      )
     ),
-    data.frame(
-      series = "pressure",
-      timestamp = strftime(PAM_data$pressure$date , "%Y-%m-%dT%H:%M:%S%z"),
-      value = PAM_data$pressure$obs,
-      label = ""
-    )
-  ),paste0(file.location,file.name,".csv"), row.names=FALSE)
+    paste0(file.location, file.name, ".csv"),
+    row.names = FALSE
+  )
+
+  # no return
 }
 
 
+
+#' Read classification of activity and pressure
+#'
+#' This function read an exported csv file from trainset (https://trainset.geocene.com/) and update the PAM logger dataset
+#'
+#' @param PAM_data PAM logger dataset list (see https://github.com/KiranLDA/PAMLr for details)
+#' @param file.location Path to the folder where the labeled file is.
+#' @param file.name Name of the file.
+#' @return PAM logger dataset list updated with the new label named `class` (`PAM_data$pressure$class` and `PAM_data$acceleration$class`)
 #' @export
 trainset.read <- function(PAM_data,
                           file.location,
-                          file.name = paste0(PAM_data$id,'_act_pres')
-                          ){
-  tmp = read.csv(paste0(file.location,file.name,"-labeled.csv"))
-  # check that same size as PAM
-  PAM_data$acceleration$class = tmp$label[tmp$series=="acceleration"]
-  PAM_data$pressure$class = tmp$label[tmp$series=="pressure"]
+                          file.name = paste0(PAM_data$id, '_act_pres')) {
+
+  # Perform test
+  testthat::expect_type(PAM_data, "list")
+  testthat::expect_true("pressure" %in% names(PAM_data))
+  testthat::expect_type(PAM_data$pressure, "list")
+  testthat::expect_true("date" %in% names(PAM_data$pressure))
+  testthat::expect_true("obs" %in% names(PAM_data$pressure))
+  testthat::expect_true("acceleration" %in% names(PAM_data))
+  testthat::expect_type(PAM_data$acceleration, "list")
+  testthat::expect_true("date" %in% names(PAM_data$acceleration))
+  testthat::expect_true("act" %in% names(PAM_data$acceleration))
+  testthat::expect_true(dir.exists(file.location))
+  testthat::expect_type(file.location, "character")
+  testthat::expect_type(file.name, "character")
+  # create path if does not exit
+  if(!dir.exists(file.location)) {
+    dir.create(file.location)
+  }
+  testthat::expect_true(dir.exists(file.location))
+
+  # read the file
+  tmp = read.csv(paste0(file.location, file.name, "-labeled.csv"))
+
+  # check that the file is in the right format and same size as PAM data
+  testthat::expect_true("series" %in% names(tmp))
+  testthat::expect_length(tmp$label, length(PAM_data$acceleration$date)+length(PAM_data$pressure$date))
+
+  # Assign label value to class
+  PAM_data$acceleration$class = tmp$label[tmp$series == "acceleration"]
+  PAM_data$pressure$class = tmp$label[tmp$series == "pressure"]
+
+  # return the updated list
   PAM_data
 }
