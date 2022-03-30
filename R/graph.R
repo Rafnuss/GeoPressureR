02dcb19605dc4370b169a37c8a072c6e99d5edb6
rafnuss <rafnuss@gmail.com>
2022-03-30 19:40:23 -0400

    Add wind to graph

--- R/graph.R
+++ R/graph.R
@@ -13,6 +13,7 @@
 #' - `s`: source node (index in the 3d grid lat-lon-sta),
 #' - `t`: target node (index in the 3d grid lat-lon-sta),
 #' - `gs`: average ground speed required to make that transition (km/h)
+#' as complex number representing the E-W as real and S-N as imaginary.
 #' - `ps`: static probability of each target node
 #' - `sz`: size of the 3d grid lat-lon-sta
 #' - `equipement`: node(s) of the first sta (index in the 3d grid lat-lon-sta)
@@ -26,14 +27,14 @@
 #' `static_prob`
 #'
 #'
-#' The vignette `How to use the graph` provided an example how to prepare the
-#' data for the function and the output of this function
+#' The vignette [Basic graph](./articles/basic-graph.html) provided an example
+#' how to prepare the data for the function and the output of this function.
 #'
 #' @param static_prob list of raster containing probability map of each
 #' stationary period. The metadata of `static_prob` needs to include the flight
 #' information to the next stationary period in the metadata `flight`.
 #' @param thr_prob_percentile threshold of percentile (see explanation above)
-#' @param thr_gs threashold of groundspeed (km/h)  (see explanation above)
+#' @param thr_gs threshold of groundspeed (km/h)  (see explanation above)
 #' @return graph as a list (see description above)
 #' @export
 graph_create <- function(static_prob,
@@ -182,20 +183,37 @@ graph_create <- function(static_prob,
     t_id <- arrayInd(grt$t, sz)
 
     # compute the groundspeed for all transition
-    grt$gs <- geosphere::distGeo(
+    gs_abs <- geosphere::distGeo(
       cbind(lon[s_id[, 2]], lat[s_id[, 1]]),
       cbind(lon[t_id[, 2]], lat[t_id[, 1]])
     ) / 1000 / flight_duration[i_s]
 
     # filter the transition based on the groundspeed
-    id <- grt$gs < thr_gs
+    id <- gs_abs < thr_gs
+    if (sum(id) == 0) {
+      stop(paste0("Using the `thr_gs` of ", thr_gs, " km/h provided with the
+                  exact distance of edges, there are not any nodes left for
+                  the stationay period: ", i_s))
+    }
     grt <- grt[id, ]
+
+    # Compute the bearing of the trajectory
+    gs_bearing <- geosphere::bearingRhumb(
+      cbind(lon[s_id[id, 2]], lat[s_id[id, 1]]),
+      cbind(lon[t_id[id, 2]], lat[t_id[id, 1]])
+    )
+    # bearing is NA if gs==0, fix for computing the complex representation
+    gs_bearing[is.na(gs_bearing)] <- 0
+
+    # save groundspeed in complex notation
+    grt$gs <- gs_abs[id] * cos((gs_bearing - 90) * pi / 180) +
+      1i * gs_abs[id] * sin((gs_bearing - 90) * pi / 180)
 
     # assign the static probability of the target node (pressure * light)
     grt$ps <- static_prob_n[[i_s + 1]][grt$t - i_s * nll]
 
     # add the edges from this stationary period to all others
-    gr <- rbind(gr, grt)
+    gr[[i_s]] <- grt
 
     if (sum(id) == 0) {
       stop(paste0("Using the `thr_gs` of ", thr_gs, " km/h provided with the
@@ -204,9 +222,11 @@ graph_create <- function(static_prob,
     }
     progress_bar(sum(nds_expend_sum[seq(1, i_s)]),
       max = sum(nds_expend_sum),
-      text = paste("| sta = ", i_s, "/", nsta - 1, sep = "")
+      text = paste0("| sta = ", i_s, "/", nsta - 1)
     )
   }
+
+  gr <- do.call("rbind", gr)
 
   # Trim
   for (i in seq_len(nsta)) {
@@ -240,8 +260,278 @@ graph_create <- function(static_prob,
   grl$temporal_extent <- lapply(static_prob, function(x) {
     raster::metadata(x)$temporal_extent
   })
+  grl$flight <- lapply(static_prob, function(x) {
+    raster::metadata(x)$flight
+  })
 
   # return
+  grl
+}
+
+
+
+#' Add windspeed and airspeed
+#'
+#' Read NetCDF file downloaded on your computer and add the average
+#' windspeed experienced by the bird and the corresponding airspeed for each
+#' edge of the graph.
+#'
+#' See the vignette [Graph with wind](./articles/wind-graph.html) for
+#' explanations and example on how to download the NetCDF file.
+#'
+#' @param grl graph constructed with `graph_create()`
+#' @param pressure pressure data from a PAM logger. This data.frame needs to
+#' contains `date` as POSIXt and `obs` in hPa. It is best practice to use
+#' `pam_read()` and `pam_sta()` to build this data.frame.
+#' @param filename character of the path where to find the netCDF file.
+#' @param thr_as threshold of airspeed (km/h)
+#' @return graph as a list with windspeed and airspeed as `ws` and `as`
+#' respectively (see `graph_create()` for more detail on the graph returned)
+#' @export
+graph_add_wind <- function(grl, pressure, filename, thr_as = Inf) {
+  stopifnot(is.list(grl))
+  stopifnot(c("s", "t", "sz", "lat", "lon", "flight") %in% names(grl))
+  stopifnot(is.data.frame(pressure))
+  stopifnot("date" %in% names(pressure))
+  stopifnot(inherits(pressure$date, "POSIXt"))
+  stopifnot("obs" %in% names(pressure))
+  stopifnot(is.numeric(pressure$obs))
+  stopifnot(is.character(filename))
+  stopifnot(file.exists(paste0(filename, "1.nc")))
+  stopifnot(is.numeric(thr_as))
+  stopifnot(length(thr_as) == 1)
+  stopifnot(thr_as >= 0)
+
+  # Extract the index in lat, lon, sta from the source and target of all edges
+  s <- arrayInd(grl$s, grl$sz)
+  t <- arrayInd(grl$t, grl$sz)
+
+  # Prepare the matrix of speed to return
+  uv <- matrix(NA, nrow = length(grl$s), ncol = 2)
+
+  # Start progress bar
+  nsta <- max(unlist(lapply(grl$flight, function(x) {
+    x$sta_id
+  })))
+  nds_expend_sum <- table(s[, 3])
+  progress_bar(0,
+    max = sum(nds_expend_sum),
+    text = paste0("| sta = ", 0, "/", nsta - 1)
+  )
+
+  # Loop through the stationary period kept in the graph
+  for (i1 in seq_len(grl$sz[3])) {
+
+    # Extract the flight information from the current sta to the next one
+    # considered in the graph. It can be the next, or if some sta are skipped
+    # at construction, it can contains multiples flights
+    fl_s <- grl$flight[[i1]]
+
+    # Extract the duration of each flights.
+    fl_s_dur <- as.numeric(difftime(fl_s$end, fl_s$start, units = "hours"))
+
+    # Determine the id of edges of the graph corresponding to this/these
+    # flight(s).
+    st_id <- which(s[, 3] == i1)
+
+    # We are assuming that the bird flight as a straight line between the source
+    # and the target node of each edge.
+    # If multiple flights happen during this transition, we assume that the bird
+    # flew with a constant groundspeed during each flight, thus considering its
+    # stop-over position to be spread according to the flight duration. This
+    # does not account for habitat, so that it would assume a bird can stop over
+    # water.
+    # While we could improve this part of the code to assume constant airspeed
+    # rather than groundspeed, we suggest to create the graph considering all
+    # stopovers.
+    ratio_sta <- as.matrix(c(0, cumsum(fl_s_dur) / sum(fl_s_dur)))
+
+    # Prepare the u- and v- windspeed for each flight (row) and edge (col)
+    u_sta <- matrix(NA, nrow = length(fl_s$sta_id), ncol = length(st_id))
+    v_sta <- matrix(NA, nrow = length(fl_s$sta_id), ncol = length(st_id))
+
+    # Loop through each flight of the transition
+    for (i2 in seq_len(length(fl_s$sta_id))) {
+
+      # Find the stationary period ID from this specific flight (source)
+      i_s <- fl_s$sta_id[i2]
+
+      # Read the netCDF file
+      nc <- ncdf4::nc_open(paste0(filename, i_s, ".nc"))
+
+      # Read data from netCDF file and convert the time of data to posixt
+      time <- as.POSIXct(ncdf4::ncvar_get(nc, "time") * 60 * 60,
+        origin = "1900-01-01", tz = "UTC"
+      )
+      pres <- ncdf4::ncvar_get(nc, "level")
+      lat <- ncdf4::ncvar_get(nc, "latitude")
+      lon <- ncdf4::ncvar_get(nc, "longitude")
+
+      # Find the start and end latitude and longitude of each edge
+      lat_s <- grl$lat[s[st_id, 1]] +
+        ratio_sta[i2] * (grl$lat[t[st_id, 1]] - grl$lat[s[st_id, 1]])
+      lon_s <- grl$lon[s[st_id, 2]] +
+        ratio_sta[i2] * (grl$lon[t[st_id, 2]] - grl$lon[s[st_id, 2]])
+      lat_e <- grl$lat[s[st_id, 1]] +
+        ratio_sta[i2 + 1] * (grl$lat[t[st_id, 1]] - grl$lat[s[st_id, 1]])
+      lon_e <- grl$lon[s[st_id, 2]] +
+        ratio_sta[i2 + 1] * (grl$lon[t[st_id, 2]] - grl$lon[s[st_id, 2]])
+
+      # As ERA5 data is available every hour, we build a one hour resolution
+      # timeserie including the start and end time of the flight. Thus, we first
+      # round the start end end time.
+      t_s <- as.POSIXct(format(fl_s$start[i2], "%Y-%m-%d %H:00:00"),
+        tz = "UTC"
+      )
+      t_e <- as.POSIXct(format(fl_s$end[i2] + 60 * 60, "%Y-%m-%d %H:00:00"),
+        tz = "UTC"
+      )
+      t_q <- seq(from = t_s, to = t_e, by = 60 * 60)
+
+      # We assume that the bird is moving with a constant groundspeed between
+      # `flight$start` and `flight$end`. Using a linear interpolation, we
+      # extract the position (lat, lon) at every hour on `t_q`. Extrapolation
+      # outside (before the bird departure or after he arrived) is with a
+      # nearest neighbor.
+
+      dt <- as.numeric(difftime(fl_s$end[i2], fl_s$start[i2], units = "hours"))
+      dlat <- (lat_e - lat_s) / dt
+      dlon <- (lon_e - lon_s) / dt
+      w <- pmax(pmin(as.numeric(
+        difftime(t_q, fl_s$start[i2], units = "hours")), dt), 0)
+      w2 <- matrix(w, nrow = length(dlat), ncol = length(w), byrow = TRUE)
+      lat_int <- lat_s + w2 * replicate(length(w), dlat)
+      lon_int <- lon_s + w2 * replicate(length(w), dlon)
+
+      # As we are interesting in the average windspeed experienced during the
+      # entire flight, we need to find the weights of each 1hr interval
+      # extracted from ERA5. We can estimate these weight assuming a linear
+      # integration of the time (trapezoidal rule) or a step integration
+      # (Riemann sum)
+
+      # Linear integration
+      w <- numeric(length(t_q))
+      stopifnot(length(w) > 2)
+      alpha <- 1 - as.numeric(difftime(fl_s$start[i2], t_q[1],
+        units = "hours"
+      ))
+      stopifnot(alpha >= 0 & alpha <= 1)
+      w[c(1, 2)] <- w[c(1, 2)] + c(alpha, 1 - alpha) * alpha
+      alpha <- 1 - as.numeric(difftime(utils::tail(t_q, 1), fl_s$end[i2],
+        units = "hours"
+      ))
+      stopifnot(alpha >= 0 & alpha <= 1)
+      w[length(w) - c(1, 0)] <- w[length(w) - c(1, 0)] +
+        c(1 - alpha, alpha) * alpha
+
+      if (length(w) >= 4) {
+        w[c(2, length(w) - 1)] <- w[c(2, length(w) - 1)] + 0.5
+      }
+      if (length(w) >= 5) {
+        w[seq(3, length(w) - 2)] <- w[seq(3, length(w) - 2)] + 1
+      }
+
+      # step integration
+      # w <- difftime(pmin(pmax(t_q+60*60/2,fl_s$start[i2]),fl_s$end[i2]),
+      #               pmin(pmax(t_q-60*60/2,fl_s$start[i2]),fl_s$end[i2]),
+      #               units = "hours")
+
+      # Prepare the interpolated u- v- vector for each flight
+      u_int <- matrix(NA, nrow = length(t_q), ncol = length(st_id))
+      v_int <- matrix(NA, nrow = length(t_q), ncol = length(st_id))
+
+      # Loop through the 1hr interval
+      for (i3 in seq_len(length(t_q))) {
+
+        # find the time step to query in ERA5
+        id_time <- which(time == t_q[i3])
+        # find the two pressure level to query (one above, one under) based on
+        # the geolocator pressure at this timestep
+        pres_obs <- pressure$obs[pressure$date == t_q[i3]]
+        df <- pres_obs - pres
+        df[df < 0] <- NA
+        id_pres <- which.min(df)
+        # if the pressure is higher than the highest level
+        # (i.e. bird below the ground level pressure), we extract only the last
+        # layer
+        n_pres <- ifelse(id_pres == length(df), 1, 2)
+
+        dlon <- lon[2] - lon[1]
+        id_lon <- which(lon >= (min(lon_int[, i3]) - dlon) &
+                          (max(lon_int[, i3]) + dlon) >= lon)
+
+        dlat <- abs(lat[2] - lat[1])
+        id_lat <- which(lat >= (min(lat_int[, i3]) - dlat) &
+                          (max(lat_int[, i3]) + dlat) >= lat)
+
+
+        # get the two maps of u- and v-
+        u <- ncdf4::ncvar_get(nc, "u",
+          start = c(id_lon[1], id_lat[1], id_pres, id_time),
+          count = c(length(id_lon), length(id_lat), n_pres, 1)
+        )
+        v <- ncdf4::ncvar_get(nc, "v",
+          start = c(id_lon[1], id_lat[1], id_pres, id_time),
+          count = c(length(id_lon), length(id_lat), n_pres, 1)
+        )
+
+        # Interpolate linearly the map of wind based on pressure.
+        if (n_pres == 2) {
+          w2 <- abs(pres[id_pres + c(0, 1)] - pres_obs)
+          w2 <- w2 / sum(w2)
+          u <- w2[1] * u[, , 1] + w2[2] * u[, , 2]
+          v <- w2[1] * v[, , 1] + w2[2] * v[, , 2]
+        }
+
+        # Interpolation the u- and v- component at the interpolated position at
+        # the current time step.
+        ll_int <- round(cbind(lat_int[, i3], lon_int[, i3]), 1)
+        ll_int_uniq <- unique(ll_int)
+        id_uniq <- match(ll_int[, 1] * 1000 + ll_int[, 2],
+                         ll_int_uniq[, 1] * 1000 + ll_int_uniq[, 2])
+
+        tmp <- pracma::interp2(rev(lat[id_lat]), lon[id_lon],
+          u[, rev(seq_len(ncol(u)))],
+          ll_int_uniq[, 1], ll_int_uniq[, 2],
+          method = "linear"
+        )
+        u_int[i3, ] <- tmp[id_uniq]
+        tmp <- pracma::interp2(rev(lat[id_lat]), lon[id_lon],
+          v[, rev(seq_len(ncol(u)))],
+          ll_int_uniq[, 1], ll_int_uniq[, 2],
+          method = "linear"
+        )
+        v_int[i3, ] <- tmp[id_uniq]
+      }
+      # Compute the average wind component of the flight accounting for the
+      # weighting scheme
+      u_sta[i2, ] <- colSums(u_int * w)
+      v_sta[i2, ] <- colSums(v_int * w)
+
+      progress_bar(sum(nds_expend_sum[seq(1, i_s)]),
+        max = sum(nds_expend_sum),
+        text = paste0("| sta = ", i_s, "/", nsta - 1)
+      )
+    }
+    # Compute the average  over all the flight of the transition accounting for
+    # the duration of the flight.
+    uv[st_id, 1] <- colSums(u_sta * fl_s_dur / sum(fl_s_dur))
+    uv[st_id, 2] <- colSums(v_sta * fl_s_dur / sum(fl_s_dur))
+  }
+
+  # save windspeed in complex notation
+  grl$ws <- uv[st_id, 1] + 1i * uv[st_id, 2]
+
+  # compute airspeed
+  grl$as <- grl$gs - grl$ws
+
+  # filter edges based on airspeed
+  id <- abs(grl$as) <= thr_as
+  grl$s <- grl$s[id]
+  grl$t <- grl$t[id]
+  grl$gs <- grl$gs[id]
+  grl$as <- grl$as[id]
+
   grl
 }
 
