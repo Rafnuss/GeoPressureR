a7bda9443ef1ac8b4fccd500977455ecda45b1b8
rafnuss <rafnuss@gmail.com>
2022-08-29 14:41:53 -0400

    rename avonet script

--- data-raw/avonet.R
+++ data-raw/avonet.R
@@ -1,5 +1,4 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `avonet` dataset goes here
 
 avonet <- utils::read.csv("data-raw/avonet_clements.csv")
-
 usethis::use_data(avonet, overwrite = TRUE)
