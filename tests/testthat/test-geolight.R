10fda2eeaff12f85d854d9e77b6993cc4b82992c
rafnuss <rafnuss@gmail.com>
2022-09-06 20:53:39 -0400

    replace `pam_data` with `pam`

--- tests/testthat/test-geolight.R
+++ tests/testthat/test-geolight.R
@@ -1,12 +1,12 @@
 library(testthat)
 library(GeoPressureR)
 
-pam_data <- pam_read(
+pam <- pam_read(
   pathname = system.file("extdata/0_PAM/18LX", package = "GeoPressureR"),
   crop_start = "2017-06-20", crop_end = "2018-05-02"
 )
 
-twl <- find_twilights(pam_data$light, shift_k = 0)
+twl <- find_twilights(pam$light, shift_k = 0)
 test_that("Check find_twilights()", {
   expect_true(all(c("twilight", "rise") %in% names(twl)))
   expect_true(nrow(twl) > 0)
