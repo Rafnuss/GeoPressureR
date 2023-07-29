ada61881df674aaff0a3c75f8abeb435cc97e6a4
rafnuss <rafnuss@gmail.com>
2023-07-29 22:07:56 +0200

    Create `trainset_write()`

--- R/trainset_read.R
+++ R/trainset_read.R
@@ -1,66 +1,3 @@
-#' Write data.frame into a csv file compatible with TRAINSET
-#'
-#' This function writes the csv file which can be open with TRAINSET
-#' (https://trainset.geocene.com/). See https://trainset.geocene.com/help for details.
-#'
-#' @param df Data.frame written in csv. Columns name defined by the parameters below.
-#' @param file Name of the label file to be saved.
-#' @param series Character or vector to be used as name for series.
-#' @param timestamp Column name of `df` to be used as timestamp.
-#' @param value Column name of `df` to be used as value.
-#' @param label Column name of `df` to be used as label. If column doesn't exist, use empty
-#' characters.
-#' @seealso [`tag_label_write()`]
-#' @noRd
-trainset_write <- function(df,
-                           file,
-                           series = "series",
-                           timestamp = "date",
-                           value = "value",
-                           label = "label") {
-  assertthat::assert_that(is.data.frame(df))
-  assertthat::assert_that(assertthat::has_name(df, c(timestamp, value)))
-  assertthat::assert_that(is.character(file))
-
-  # Create empty label if label doesn't exit
-  if (!assertthat::has_name(df, label)) {
-    df[[label]] <- ""
-  }
-
-  # Remove row with empty element
-  df <- df[!is.na(df[[value]]), ]
-
-  # Combine the variable
-  df_trainset <- data.frame(
-    series = df[[series]],
-    timestamp = strftime(df[[timestamp]], "%Y-%m-%dT%H:%M:%SZ", tz = "UTC"),
-    value = df[[value]],
-    label = df[[label]]
-  )
-
-  # Check if folder exist
-  dir_file <- dirname(file)
-  if (!assertthat::is.dir(dir_file)) {
-    cli::cli_alert_warning("The directory {.file {dir_file}} does not exists.")
-    res <- utils::askYesNo("Do you want to create it?")
-    if (res) {
-      dir.create(dir_file)
-    } else {
-      return(FALSE)
-    }
-  }
-
-  # write a combined data.frame of pressure and acceleration in csv.
-  utils::write.csv(
-    df_trainset,
-    file = file,
-    row.names = FALSE
-  )
-
-  cli::cli_alert_success("{.file {file}} written successfully.")
-  return(file)
-}
-
 
 #' Read trainset csv file and update a data.frame
 #'
@@ -76,7 +13,7 @@ trainset_write <- function(df,
 #' @noRd
 trainset_read <- function(df,
                           file,
-                          series = NA,
+                          series = NULL,
                           timestamp = "date",
                           label = "label") {
   assertthat::assert_that(is.data.frame(df))
@@ -98,7 +35,7 @@ trainset_read <- function(df,
   csv$date <- strptime(csv$timestamp, "%FT%T", tz = "UTC")
 
   # Extract only data from the corresponding series
-  if (!is.na(series)) {
+  if (!is.null(series)) {
     csv <- csv[csv$series == series, ]
   }
 
@@ -123,7 +60,7 @@ trainset_read <- function(df,
   missing_pres <- sum(is.na(id_match))
 
   if (missing_pres > 0) {
-    series_name <- ifelse(is.na(series), "", glue::glue(" of ", series))
+    series_name <- ifelse(is.null(series), "", glue::glue(" of ", series))
     cli::cli_warn(c(
       i = "The labelization file{series_name} is missing {missing_pres} timesteps and includes
       {nrow(csv) - nrow(df) + missing_pres} timestep which are not nedded. ",
---
+++ R/trainset_write.R
@@ -0,0 +1,64 @@
+#' Write data.frame into a csv file compatible with TRAINSET
+#'
+#' This function writes the csv file which can be open with TRAINSET
+#' (https://trainset.geocene.com/). See https://trainset.geocene.com/help for details.
+#'
+#' @param df Data.frame written in csv. Columns name defined by the parameters below.
+#' @param file Name of the label file to be saved.
+#' @param series Character or vector to be used as name for series.
+#' @param timestamp Column name of `df` to be used as timestamp.
+#' @param value Column name of `df` to be used as value.
+#' @param label Column name of `df` to be used as label. If column doesn't exist, use empty
+#' characters.
+#' @seealso [`tag_label_write()`]
+#' @noRd
+trainset_write <- function(df,
+                           file,
+                           series = "series",
+                           timestamp = "date",
+                           value = "value",
+                           label = "label") {
+  assertthat::assert_that(is.data.frame(df))
+  assertthat::assert_that(assertthat::has_name(df, c(timestamp, value)))
+  assertthat::assert_that(is.character(file))
+
+  # Create empty label if label doesn't exit
+  if (!assertthat::has_name(df, label)) {
+    df[[label]] <- ""
+  }
+
+  # Remove row with empty element
+  df <- df[!is.na(df[[value]]), ]
+
+  # Combine the variable
+  df_trainset <- data.frame(
+    series = df[[series]],
+    timestamp = strftime(df[[timestamp]], "%Y-%m-%dT%H:%M:%SZ", tz = "UTC"),
+    value = df[[value]],
+    label = df[[label]]
+  )
+
+  # Check if folder exist
+  dir_file <- dirname(file)
+  if (!dir.exists(dir_file)) {
+    cli::cli_alert_warning("The directory {.file {file.path(getwd(), dir_file)}} does not exists.")
+    res <- utils::askYesNo("Do you want to create it?")
+    if (res) {
+      dir.create(dir_file)
+    } else {
+      return(FALSE)
+    }
+  }
+
+  # write a combined data.frame of pressure and acceleration in csv.
+  utils::write.csv(
+    df_trainset,
+    file = file,
+    row.names = FALSE
+  )
+
+  cli::cli_alert_success("{.file {file}} written successfully.")
+  return(file)
+}
+
+
