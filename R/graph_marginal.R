225fd62bdd479427560d6ce79a6c9f7584ace0ec
rafnuss <rafnuss@gmail.com>
2023-07-29 22:15:57 +0200

    split graph products on each file

---
+++ R/graph_marginal.R
@@ -0,0 +1,81 @@
+#' Marginal probability map
+#'
+#' Compute the marginal probability map from a graph. The computation uses the [forward
+#' backward algorithm](https://en.wikipedia.org/wiki/Forward%E2%80%93backward_algorithm). See
+#' section 2.3.2 in Nussbaumer (2023) for more information.
+#'
+#' @param graph graph constructed with [`graph_create()`].
+#' @return A list of the marginal maps for each stationary period (even those not modelled). Best to
+#' include within `tag`.
+#'
+#' @seealso [GeoPressureManual | Basic graph](
+#' https://raphaelnussbaumer.com/GeoPressureManual/basic-graph.html#output-2-marginal-probability-map)
+#' @references{ Nussbaumer, Raphaël, Mathieu Gravey, Martins Briedis, Felix Liechti, and Daniel
+#' Sheldon. 2023. “Reconstructing bird trajectories from pressure and wind data using a highly
+#' optimized hidden Markov model.” *Methods in Ecology and Evolution*.
+#' <https://doi.org/10.1111/2041-210X.14082>.}
+#' @family graph
+#' @export
+graph_marginal <- function(graph) {
+  graph_assert(graph, "full")
+
+  # Compute the transition matrix (movement model)
+  transition <- graph_transition(graph)
+
+  # number of nodes in the 3d grid
+  n <- prod(graph$sz)
+
+  # matrix of transition * observation
+  trans_obs <- Matrix::sparseMatrix(graph$s, graph$t,
+                                    x = transition * graph$obs[graph$t], dims = c(n, n)
+  )
+
+  # Initiate the forward probability vector (f_k^T in Nussbaumer et al. (2023) )
+  map_f <- Matrix::sparseMatrix(1, 1, x = 0, dims = c(1, n))
+
+  # Initiate the backward probability vector (b_k in Nussbaumer et al. (2023) )
+  map_b <- Matrix::sparseMatrix(1, 1, x = 0, dims = c(n, 1))
+
+  # build iteratively the marginal probability backward and forward by re-using the mapping
+  # computed for previous stationary period. Set the equipment and retrieval site in each loop
+  for (i_s in seq_len(graph$sz[3] - 1)) {
+    map_f[1, graph$equipment] <- graph$obs[graph$equipment] # P_0^T O_0 with P_0=1
+    map_f <- map_f %*% trans_obs # Eq. 3 in Nussbaumer et al. (2023)
+
+    map_b[graph$retrieval, 1] <- 1 # equivalent to map_b[, 1] <- 1 but slower
+    map_b <- trans_obs %*% map_b # Eq. 3 in Nussbaumer et al. (2023)
+  }
+  # add the retrieval and equipment at the end to finish it
+  map_f[1, graph$equipment] <- graph$obs[graph$equipment]
+  map_b[graph$retrieval, 1] <- 1
+
+  # combine the forward and backward
+  map_fb <- map_f * Matrix::t(map_b) # Eq. 5 in Nussbaumer et al. (2023)
+
+  # reshape mapping as a full (non-sparce matrix of correct size)
+  map_fb <- as.matrix(map_fb)
+  dim(map_fb) <- graph$sz
+
+  # return as list
+  marginal <- vector("list", nrow(graph$stap))
+  stap_includeed <- graph$stap$stap_id[graph$stap$include]
+  for (i_s in seq_len(graph$sz[3])) {
+    map_fb_i <- map_fb[, , i_s]
+    map_fb_i[graph$mask_water] <- NA
+    if (sum(map_fb_i, na.rm = TRUE) == 0) {
+      cli::cli_abort(c(
+        x = "The probability of some transition are too small to find numerical solution.",
+        i = "Please check the data used to create the graph."
+      ))
+    }
+    marginal[[stap_includeed[i_s]]] <- map_fb_i
+  }
+
+  # Add attribute
+  attr(marginal, "id") <- graph$id
+  attr(marginal, "extent") <- graph$extent
+  attr(marginal, "scale") <- graph$scale
+  attr(marginal, "stap") <- graph$stap
+
+  return(marginal)
+}
---
+++ R/graph_simulation.R
@@ -0,0 +1,123 @@
+#' Simulation of trajectories
+#'
+#' @description
+#' This function randomly simulates multiple trajectories from a graph using the forward filtering
+#' backward sampling algorithm. See section 2.3.3 in Nussbaumer (2023) for more information.
+#'
+#' @param graph A graph object.
+#' @param nj Number of simulations.
+#' @return Path data.frame containing the columns
+#' -`stap_id` stationary period
+#' - `j` unique ID for each simulation.
+#' - `ind` indices of the coordinate in the 2D grid. Useful to retrieve map or graph information
+#' - `lat` latitude,
+#' - `lon` longitude
+#' - `start` datetime of the start of the stationary period (same as in `stap`)
+#' - `end` datetime of the end of the stationary period (same as in `stap`)
+#' - `include` logical if stationary period was modeled (same as in `stap`)
+#' - `nb_sample known` number of datapoint used to compute pressure (same as in `stap`)
+#'
+#' @seealso [GeoPressureManual | Basic graph](
+#' https://raphaelnussbaumer.com/GeoPressureManual/basic-graph.html#output-3-simulate-path)
+#' @references{ Nussbaumer, Raphaël, Mathieu Gravey, Martins Briedis, Felix Liechti, and Daniel
+#' Sheldon. 2023. “Reconstructing bird trajectories from pressure and wind data using a highly
+#' optimized hidden Markov model.” *Methods in Ecology and Evolution*.
+#' <https://doi.org/10.1111/2041-210X.14082>.}
+#' @family graph
+#' @export
+graph_simulation <- function(graph,
+                             nj = 10) {
+  graph_assert(graph, "full")
+
+  # Compute the matrix TO
+  trans_obs <- graph_transition(graph) * graph$obs[graph$t]
+
+  # number of nodes in the 3d grid
+  n <- prod(graph$sz)
+  nll <- graph$sz[1] * graph$sz[2]
+
+  # Find the stationary index of all the source so that only the edges from a specific stationary
+  # period can be easily query
+  s_id <- arrayInd(graph$s, graph$sz)
+
+  # As we will simulate in forward chronological order, we will be able to create map_f inside the
+  # simulation. However, map_b needs to be computed for all stationary period in advance, starting
+  # by the last stationary period and moving backward in time as follow.
+  # We store map_b as a list of vector of size lat x lon (instead of 3D with stap)
+  map_b <- list()
+
+  # Initiate map_b at the last stap with the retrieval node (b_n=1 in Nussbaumer et al. 2023)
+  map_b[[graph$sz[3]]] <- Matrix::sparseMatrix(
+    rep(1, length(graph$retrieval)),
+    graph$retrieval,
+    x = 1, dims = c(1, n)
+  )
+
+  # Build all map_b in backward order
+  for (i_s in (graph$sz[3] - 1):1) {
+    id <- s_id[, 3] == i_s
+    map_b[[i_s]] <- map_b[[i_s + 1]] %*%
+      Matrix::sparseMatrix(graph$t[id], graph$s[id], x = trans_obs[id], dims = c(n, n))
+    # Same as Eq. 3 in Nussbaumer et al. (2023) but with b_k transpose thus TO * b_k instead
+    # of b_k * TO
+  }
+
+  # Initialize the path, stored as index of the 3D grid
+  path_ind3d <- matrix(ncol = graph$sz[3], nrow = nj)
+
+  # Sample the first position with map_b and map_f as f_0 = P_0 * O_0
+  map_f_0 <- Matrix::sparseMatrix(1, 1, x = 0, dims = c(1, n))
+  map_f_0[graph$equipment] <- graph$obs[graph$equipment]
+  map_fb <- map_b[[1]][1:nll] * map_f_0[1:nll]
+
+  for (i_j in seq_len(nj)) {
+    path_ind3d[i_j, 1] <- sum(stats::runif(1) > cumsum(map_fb) / sum(map_fb)) + 1
+  }
+
+  # Loop through the simulation along chronological order
+  cli::cli_progress_bar(total = graph$sz[3])
+  for (i_s in seq(2, graph$sz[3])) {
+    # find edges arriving to this stationary period
+    id <- s_id[, 3] == (i_s - 1)
+
+    # create the local trans_obs (only edges from previous stap to this stap
+    trans_obs_l <- Matrix::sparseMatrix(graph$s[id], graph$t[id], x = trans_obs[id], dims = c(n, n))
+
+    # build the forward mapping from the simulated nodes of the previous stationary period to the
+    # current one using trans_obs_l
+    map_f <- Matrix::sparseMatrix(seq_len(nj), path_ind3d[, i_s - 1], x = 1, dims = c(nj, n)) %*%
+      trans_obs_l
+
+    # Combine forward and backward and samples
+    if (nj > 1) {
+      path_ind2d <- apply(map_f[, nll * (i_s - 1) + (1:nll)], 1, function(map_f_i) {
+        map_fb <- map_f_i * map_b[[i_s]][nll * (i_s - 1) + (1:nll)]
+        sum(stats::runif(1) > cumsum(map_fb) / sum(map_fb)) + 1
+      })
+    } else {
+      map_fb <- map_f[, nll * (i_s - 1) + (1:nll)] * map_b[[i_s]][nll * (i_s - 1) + (1:nll)]
+      path_ind2d <- sum(stats::runif(1) > cumsum(map_fb) / sum(map_fb)) + 1
+    }
+
+    # Convert ids into 3D coordinates
+    path_ind3d[, i_s] <- path_ind2d + nll * (i_s - 1)
+
+    # Update progress bar
+    cli::cli_progress_update(set = i_s, force = TRUE)
+  }
+
+  # convert 3D to 2D grid
+  path_ind2d <- path_ind3d - t(replicate(nj, nll * (seq_len(graph$sz[3]) - 1)))
+
+  # Path was defined on the modeled stationary period which might be different than the full stap,
+  # but we want to generate path at the level of all stationary periods
+  path_ind2d_full <- matrix(ncol = nrow(graph$stap), nrow = nj)
+  # find the stap_id of the model
+  stap_includeed <- graph$stap$stap_id[graph$stap$include]
+  path_ind2d_full[, stap_includeed] <- path_ind2d
+
+  # Convert the index of the path in a path data.frame
+  path <- ind2path(path_ind2d_full, graph)
+
+  return(path)
+}
