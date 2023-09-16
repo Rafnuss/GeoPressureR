99fd8ef35d66956a26924cbce2ac12156df7c5de
rafnuss <rafnuss@gmail.com>
2023-09-16 07:24:08 +0200

    fix cmd-check error

--- R/geopressureviz.R
+++ R/geopressureviz.R
@@ -49,10 +49,12 @@ geopressureviz <- function(x,
       if (!is.null(marginal)) {
         marginal0 <- marginal
       }
+      # Avoid CMD error
+      path_most_likely <- NULL
       # Load interim data
       load(file)
       # Accept path_most_likely instead of path
-      if (exists("path_most_likely")) {
+      if (!is.null("path_most_likely")) {
         path <- path_most_likely
       }
       # Overwrite loaded variable with arguments if provided
