b019b255707c0351440e8aab7aa2d196611121ea
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-01-20 19:29:08 +0100

    Add reading of `BAS` tags

--- R/tag_create.R
+++ R/tag_create.R
@@ -27,6 +27,9 @@
 #'    - `pressure_file = "*.deg"`
 #'    - `light_file = "*.lux"` (optional)
 #'    - `acceleration_file = "*.deg"` (optional)
+#' - British Antarctic Survey (`bas`), aquired by Biotrack Ltd in 2011, [renamed Lotek in 2019
+#' ](https://www.lotek.com/about-us/history/). Only works for light data (`assert_pressure = FALSE`)
+#'    - `light_file = "*.lig"`
 #' - [Lund CAnMove (`lund`)](https://bit.ly/3P6quyi)
 #'    - `pressure_file = "*_press.xlsx"`
 #'    - `light_file = "*_acc.xlsx"` (optional)
@@ -53,7 +56,7 @@
 #' or post-retrieval data.
 #'
 #' @param id unique identifier of a tag.
-#' @param manufacturer One of `NULL`, `"soi"`, `"migratetech"`, `"lund"` or `"manual"`
+#' @param manufacturer One of `NULL`, `"soi"`, `"migratetech"`, `"bas"`, `"lund"` or `"manual"`
 #' @param directory path of the directory where the tag files can be read.
 #' @param pressure_file name of the file with pressure data. Full pathname  or finishing
 #' with extensions (e.g., `"*.pressure"`, `"*.deg"` or `"*_press.xlsx"`).
@@ -156,6 +159,8 @@ tag_create <- function(id,
         manufacturer <- "soi"
       } else if (any(grepl("\\.deg$", list.files(directory)))) {
         manufacturer <- "migratetech"
+      } else if (any(grepl("\\.lig$", list.files(directory)))) {
+        manufacturer <- "bas"
       } else if (any(grepl("_press\\.xlsx$", list.files(directory)))) {
         manufacturer <- "lund"
       } else {
@@ -169,7 +174,7 @@ tag_create <- function(id,
     }
   }
   assertthat::assert_that(is.character(manufacturer))
-  manufacturer_possible <- c("auto", "datapackage", "soi", "migratetech", "lund", "manual")
+  manufacturer_possible <- c("auto", "datapackage", "soi", "migratetech", "bas", "lund", "manual")
   if (!any(manufacturer %in% manufacturer_possible)) {
     cli::cli_abort(c(
       "x" = "{.var manufacturer} needs to be one of {.val {manufacturer_possible}}"
@@ -209,6 +214,13 @@ tag_create <- function(id,
       directory = directory,
       deg_file = pressure_file,
       light_file = light_file,
+      quiet = quiet
+    )
+  } else if (manufacturer == "bas") {
+    tag <- tag_create_bas(
+      tag,
+      directory = directory,
+      lig_file = light_file,
       quiet = quiet
     )
   } else if (manufacturer == "lund") {
@@ -602,6 +614,52 @@ tag_create_migratetech <- function(tag,
   # Add parameter information
   tag$param$tag_create$pressure_file <- deg_path
   tag$param$tag_create$light_file <- light_path
+
+  return(tag)
+}
+
+
+#' Read British Antarctic Survey
+#'
+#' BASTrak (.lig) files
+#' Each line represents a ten minute period. Errors are shown on additional lines.
+#' <ok/suspect>,<DD/MM/YY hh:mm:ss>,<seconds reference>,<light>
+#' where
+#' - <ok/suspect> indicates whether the data is ok or suspect
+#' - <DD/MM/YY hh:mm:ss> is the time stamp
+#' - <seconds reference> is another way of representing the time; it is the number of
+#' seconds elapsed since the reference chosen when the file was processed
+#' - <light> is the maximum light value measured during the previous 10 minutes
+#' @noRd
+tag_create_bas <- function(tag,
+                           directory,
+                           lig_file = NULL,
+                           act_file = NULL,
+                           quiet) {
+  # Read Pressure
+  if (is.null(lig_file)) {
+    lig_file <- "*.lig"
+  }
+  lig_path <- tag_create_detect(lig_file, directory, quiet = quiet)
+  if (is.null(lig_path)) {
+    cli::cli_abort(c(
+      "x" = "There are no file {.val {lig_file}}",
+      "!" = "{.var lig_file} is required!"
+    ))
+  }
+
+  # Read file
+  data_raw <- utils::read.delim(lig_path, sep = ",", header = FALSE)
+  tag$light <- data.frame(
+    date = as.POSIXct(strptime(data_raw[, 2],
+      tz = "UTC",
+      format = "%d/%m/%y %H:%M:%S"
+    )),
+    value = data_raw[, 4]
+  )
+
+  # Add parameter information
+  tag$param$tag_create$light_file <- lig_file
 
   return(tag)
 }
