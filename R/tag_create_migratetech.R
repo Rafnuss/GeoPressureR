97c7360df1e5404246c04bcb441f56a93b783352
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-11-10 21:03:26 +0100

    Don't make pressure required in tag_create_migratetech by default

--- R/tag_create_migratetech.R
+++ R/tag_create_migratetech.R
@@ -18,88 +18,84 @@ tag_create_migratetech <- function(
     deg_file <- "*.deg"
   }
   deg_path <- tag_create_detect(deg_file, directory, quiet = quiet)
-  if (is.null(deg_path)) {
-    cli::cli_abort(c(
-      "x" = "There are no file {.val {deg_file}}",
-      "!" = "{.var deg_file} is required!"
+  if (!is.null(deg_path)) {
+    assertthat::assert_that(grepl(
+      "Migrate Technology",
+      readLines(deg_path, n = 1)
     ))
-  }
-  assertthat::assert_that(grepl(
-    "Migrate Technology",
-    readLines(deg_path, n = 1)
-  ))
-  line2 <- readLines(deg_path, n = 2)[[2]]
-  v <- regmatches(line2, regexpr("Type: \\K\\d+", line2, perl = TRUE))
-  if (v < 13) {
-    cli::cli_abort(
-      "The deg file {.file {deg_path}} is not compatible. Line 2 should \\
+    line2 <- readLines(deg_path, n = 2)[[2]]
+    v <- regmatches(line2, regexpr("Type: \\K\\d+", line2, perl = TRUE))
+    if (v < 13) {
+      cli::cli_abort(
+        "The deg file {.file {deg_path}} is not compatible. Line 2 should \\
                contains {.val Type:x}, with x>=13."
+      )
+    }
+    # Retrieve full model number
+    tag$param$migratec_model <- regmatches(
+      line2,
+      regexpr("Type: \\K[\\d.]+", line2, perl = TRUE)
     )
-  }
-  # Retrieve full model number
-  tag$param$migratec_model <- regmatches(
-    line2,
-    regexpr("Type: \\K[\\d.]+", line2, perl = TRUE)
-  )
-  # Check for drift
-  line16 <- readLines(deg_path, n = 16)[[16]]
-  drift <- abs(
-    as.numeric(regmatches(line16, regexpr("-?\\d+\\.\\d*", line16))) / 60
-  )
-  if (drift > 30) {
-    cli::cli_warn(c(
-      "!" = "The deg file {.file {deg_path}} is recording a drift of {format_minutes(drift)} \\
+    # Check for drift
+    line16 <- readLines(deg_path, n = 16)[[16]]
+    drift <- abs(
+      as.numeric(regmatches(line16, regexpr("-?\\d+\\.\\d*", line16))) / 60
+    )
+    if (drift > 30) {
+      cli::cli_warn(c(
+        "!" = "The deg file {.file {deg_path}} is recording a drift of {format_minutes(drift)} \\
       (line 16) which seems suspicious.",
-      ">" = "Check for error (e.g. timezone)"
-    ))
-  }
-  # Find column index with pressure
-  hdr <- utils::read.delim(
-    deg_path,
-    skip = 19,
-    nrow = 1,
-    header = FALSE,
-    sep = ""
-  )
-  col <- which(hdr == "P(Pa)")
-  if (length(col) > 0) {
-    # Read file
-    tag$pressure <- tag_create_dto(
+        ">" = "Check for error (e.g. timezone)"
+      ))
+    }
+    # Find column index with pressure
+    hdr <- utils::read.delim(
       deg_path,
-      skip = 20,
-      col = col,
-      date_format = "%d/%m/%Y %H:%M:%S",
-      quiet = quiet
+      skip = 19,
+      nrow = 1,
+      header = FALSE,
+      sep = ""
     )
-    # convert Pa in hPa
-    tag$pressure$value <- tag$pressure$value / 100
-  }
+    col <- which(hdr == "P(Pa)")
+    if (length(col) > 0) {
+      # Read file
+      tag$pressure <- tag_create_dto(
+        deg_path,
+        skip = 20,
+        col = col,
+        date_format = "%d/%m/%Y %H:%M:%S",
+        quiet = quiet
+      )
+      # convert Pa in hPa
+      tag$pressure$value <- tag$pressure$value / 100
+    }
 
-  # Read acceleration
-  col <- which(hdr == "Zact")
-  if (length(col) != 0) {
-    # Read file
-    tag$acceleration <- tag_create_dto(
-      deg_path,
-      skip = 20,
-      col = col,
-      date_format = "%d/%m/%Y %H:%M:%S",
-      quiet = quiet
-    )
-    tag$param$tag_create$acceleration_file <- deg_path
-  }
+    # Read acceleration
+    col <- which(hdr == "Zact")
+    if (length(col) != 0) {
+      # Read file
+      tag$acceleration <- tag_create_dto(
+        deg_path,
+        skip = 20,
+        col = col,
+        date_format = "%d/%m/%Y %H:%M:%S",
+        quiet = quiet
+      )
+      tag$param$tag_create$acceleration_file <- deg_path
+    }
 
-  # Read temperature
-  col <- which(hdr == "T('C)")
-  if (length(col) != 0) {
-    tag$temperature <- tag_create_dto(
-      deg_path,
-      skip = 20,
-      col = col,
-      date_format = "%d/%m/%Y %H:%M:%S",
-      quiet = quiet
-    )
-    tag$param$tag_create$temperature_external_file <- deg_path
+    # Read temperature
+    col <- which(hdr == "T('C)")
+    if (length(col) != 0) {
+      tag$temperature <- tag_create_dto(
+        deg_path,
+        skip = 20,
+        col = col,
+        date_format = "%d/%m/%Y %H:%M:%S",
+        quiet = quiet
+      )
+      tag$param$tag_create$temperature_external_file <- deg_path
+    }
   }
 
   # Read light
